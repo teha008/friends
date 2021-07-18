138d16e878e81380aac9fd4a663f280109462ca2
조상호 컴퓨터 <teha007@naver.com>
2021-07-19 00:08:27 +0900

    루비온레일즈 친구 관리 앱 만들기

---
+++ README.md
@@ -0,0 +1,27 @@
+[작성일]
+2021년 7월 17일
+
+# README
+
+This README would normally document whatever steps are necessary to get the
+application up and running.
+
+Things you may want to cover:
+
+* Ruby version
+
+* System dependencies
+
+* Configuration
+
+* Database creation
+
+* Database initialization
+
+* How to run the test suite
+
+* Services (job queues, cache servers, search engines, etc.)
+
+* Deployment instructions
+
+* ...
---
+++ app/controllers/friends_controller.rb
@@ -0,0 +1,76 @@
+class FriendsController < ApplicationController
+  before_action :set_friend, only: %i[ show edit update destroy ]
+  before_action :authenticate_user!, except: [:index, :show]
+  before_action :correct_user, only: [:edit, :update, :destroy]
+  
+  # GET /friends or /friends.json
+  def index
+    @friends = Friend.all
+  end
+
+  # GET /friends/1 or /friends/1.jsonuser_id
+  def show
+  end
+
+  # GET /friends/new
+  def new
+    @friend = Friend.new
+  end
+
+  # GET /friends/1/edit
+  def edit
+  end
+
+  # POST /friends or /friends.json
+  def create
+    @friend = Friend.new(friend_params)
+
+    respond_to do |format|
+      if @friend.save
+        format.html { redirect_to @friend, notice: "친구정보 등록완료" }
+        format.json { render :show, status: :created, location: @friend }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @friend.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PATCH/PUT /friends/1 or /friends/1.json
+  def update
+    respond_to do |format|
+      if @friend.update(friend_params)
+        format.html { redirect_to @friend, notice: "친구정보 수정완료" }
+        format.json { render :show, status: :ok, location: @friend }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+        format.json { render json: @friend.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /friends/1 or /friends/1.json
+  def destroy
+    @friend.destroy
+    respond_to do |format|
+      format.html { redirect_to friends_url, notice: "친구정보 삭제완료" }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_friend
+      @friend = Friend.find(params[:id])
+    end
+
+    def correct_user_
+      @friend = current_user.friends.find_by(id: params[:id])
+      redirect_to_friends_path, notice: "이 친구를 편집할 수 있는 권한이 없습니다." if @friend.nil?
+    end
+
+    # Only allow a list of trusted parameters through.
+    def friend_params
+      params.require(:friend).permit(:first_name, :last_name, :email, :phone, :twitter, :user_id)
+    end
+end
